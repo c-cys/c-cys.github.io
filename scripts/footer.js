cb9c83bff51ac329c330c264dafd0328ab367d96
c-cys <choiys071222@gmail.com>
2024-08-29 21:55:23 +0900

    v2.0.0

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,34 +1,18 @@
-document.write(`
-    <link rel="stylesheet" href="../stylesheets/footer.css">
-    <div id="copyright">
-        <div id="copyline">
-            <img src="../assets/images/logo.png" height="40px">
-            <span class="copyright" style="font-size:20px; color:white; margin-left:5px">경기북과학고등학교 유일 환경·에너지 동아리 GLOBE</span>
-            <p class="copyright" style="font-size:17px; margin-left:50px"> Global Learning and Observations to Benefit the Environment </p>
-            <p class="copyright" style="font-size:13px; color:grey"><br>경기도 의정부시 체육로135번길 32</p>
-        </div>
-    </div>
-`);
+const linkElement = document.createElement('link');
+linkElement.rel = 'stylesheet';
+linkElement.href = '../stylesheets/footer.css';
+document.head.appendChild(linkElement);
 
-/* document.write(`
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-pEX6qRdC5QCc3+a2vEA6jngsHk83R/2zAJl9+pr8f1jV4VAvVb/uHAHR3mzrsXgT" crossorigin="anonymous">
-    <link rel="stylesheet" href="../stylesheets/footer.css">
-    
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-        <div class="col-md-4 align-items-center">
-            <div style="margin-left:30px">
-                <p></p>
-                <img src="../assets/images/logo.png" height="40" style="margin-bottom:5px"> <span style="margin-left:10px; font-size:20px">경기북과학고등학교 유일 환경·에너지 동아리 GLOBE</span><p style="color:rgb(196, 196, 196); margin-left:55px">Global Learning and Observations to Benefit the Environment</p>
-            </div>
-        </div>
-        
-        <div class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        </div>
-        
-        <div class="col-md-4 align-items-center justify-content-end">
-            <p class="mb-0">경기도 의정부시 체육로135번길 32</p>
-            <p class="mb-0">Instagram: @gbs_globe</p>
-        </div>
-    </footer>
-`); */
+const footer = document.createElement('div');
+footer.id = 'copyright';
+
+footer.innerHTML = `
+    <div id="copyline">
+        <img src="../assets/images/logo.png" height="50px">
+        <span class="copyright" style="font-size:20px; color:white; margin-left:5px">경기북과학고등학교 유일 환경·에너지 동아리 GLOBE</span>
+        <p class="copyright" style="font-size:17px; margin-left:60px">Global Learning and Observations to Benefit the Environment</p>
+        <p class="copyright" style="font-size:13px; margin-left:60px; color:grey">경기도 의정부시 체육로135번길 32</p>
+    </div>
+`;
+
+document.body.appendChild(footer);
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,46 +1,45 @@
-document.write(`
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    
-    <link rel="icon" href="../assets/images/logo.png">
-    
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <img class="navbar-brand" src="../assets/images/logo.png" width="50px" height="60px">
-            <a class="navbar-brand" href="../index.html" style="font-size:33px; font-weight:450">
-                GLOBE
-            </a>
-            
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav" style="font-size:20px">
-                <li class="nav-item mx-2 dropdown">
-                    <a class="nav-link" href="../pages/calendar.html">2024 활동 계획</a>
-                </li>
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarHtml = `
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+                <img class="navbar-brand" src="../assets/images/logo.png" width="50px" height="60px">
+                <a class="navbar-brand" href="../index.html" style="font-size:33px; font-weight:450">
+                    GLOBE
+                </a>
                 
-                <li class="nav-item mx-2 dropdown">
-                    <a class="nav-link" href="../pages/awards.html">대회 실적</a>
-                </li>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav" style="font-size:20px">
+                        <li class="nav-item mx-2 dropdown">
+                            <a class="nav-link" href="../pages/calendar.html">2024 활동 계획</a>
+                        </li>
+                        
+                        <li class="nav-item mx-2 dropdown">
+                            <a class="nav-link" href="../pages/awards.html">대회 실적</a>
+                        </li>
+                        
+                        <li class="nav-item mx-2 dropdown">
+                            <a class="nav-link" href="#" id="navbarDropdown2" role="menubar" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                세미나 자료
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                                <a class="dropdown-item" href="../pages/seminar_biology.html">생명</a>
+                                <a class="dropdown-item" href="../pages/seminar_chemistry.html">화학</a>
+                                <a class="dropdown-item" href="../pages/seminar_challenges.html">대회</a>
+                                <a class="dropdown-item" href="../pages/seminar_others.html">기타</a>
+                            </div>
+                        </li>
+                        <li class="nav-item mx-2 dropdown">
+                            <a class="nav-link" href="../pages/research.html">연구 활동</a>
+                        </li>
+                    </ul>
+                </div>
                 
-                <li class="nav-item mx-2 dropdown">
-                    <a class="nav-link" href="#" id="navbarDropdown2" role="menubar" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        세미나 자료
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
-                        <a class="dropdown-item" href="../pages/seminar_biology.html">생명</a>
-                        <a class="dropdown-item" href="../pages/seminar_chemistry.html">화학</a>
-                        <a class="dropdown-item" href="../pages/seminar_challenges.html">대회</a>
-                        <a class="dropdown-item" href="../pages/seminar_others.html">기타</a>
-                    </div>
-                </li>
-                <li class="nav-item mx-2 dropdown">
-                    <a class="nav-link" href="../pages/research.html">연구 활동</a>
-                </li>
-            </ul>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-    </nav>
-`)
+        </nav>
+    `;
+
+    document.body.insertAdjacentHTML('afterbegin', navbarHtml);
+});
